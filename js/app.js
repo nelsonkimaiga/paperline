edfe3e8a2e102d652c1c6e245cbe05542e991110
nelsonkimaiga <nelsonkimaiga@aol.com>
2017-08-29 10:36:26 +0300

    add accordon js

--- js/app.js
+++ js/app.js
@@ -1,17 +1,60 @@
-// ********************************************************
-// NAV BAR
-// 
-$(function(){
 
-    // Returns width of browser viewport
-    var browser = $(window).width();
-    // Returns width of HTML document
-    var document = $(document).width();
+$(document).foundation()
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop();
 
-    $('.nav .toggle-nav').click(function(e){
-        $('.nav .nav-mobile').addClass('style-mobile').slideToggle('slow');
-        e.preventDefault();
+    if (scroll >= 50) {
+        $(".header").addClass("scrolling");
+    } else {
+        $(".header").removeClass("scrolling");
+    }
+});
+ //EOF DOCUMENT.READY
+$(function () {
+    $('a[href*="#"]:not([href="#"])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
     });
-
 });
 
+// Returns true if the specified element has been scrolled into the viewport.
+function isElementInViewport(elem) {
+    var $elem = $(elem);
+
+    // Get the scroll position of the page.
+    var scrollElem = ((navigator.userAgent.toLowerCase().indexOf('webkit') != -1) ? 'body' : 'html');
+    var viewportTop = $(scrollElem).scrollTop();
+    var viewportBottom = viewportTop + $(window).height();
+
+    // Get the position of the element on the page.
+    var elemTop = Math.round($elem.offset().top);
+    var elemBottom = elemTop + $elem.height();
+
+    return ((elemTop < viewportBottom) && (elemBottom > viewportTop));
+}
+
+// Check if it's time to start the animation.
+function checkAnimation() {
+    var $elem = $('.timer');
+
+    // If the animation has already been started
+    if ($elem.hasClass('start')) return;
+
+    if (isElementInViewport($elem)) {
+        // Start the animation
+        $elem.addClass('start');
+    }
+}
+
+// Capture scroll events
+$(window).scroll(function () {
+    checkAnimation();
+});
